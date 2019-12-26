008f2e13e1980fe7bb239f6e5ee18de88e57fbf5
Illinux <snellemany@gmail.com>
2019-12-26 08:39:06 +0100

    Initial Commit

---
+++ __resource.lua
@@ -0,0 +1,15 @@
+client_script "client.lua"
+
+files {
+  'index.html',
+  '1.css',
+  'logo.png',
+  'music/music.ogg',
+  'app.js',
+  'background.jpg',
+}
+
+loadscreen_manual_shutdown "yes"
+loadscreen 'index.html'
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
